df5c8a7e7abf5af372e4ccf70332469aba99b343
jmfloresmx <jmfloresa@uaemex.mx>
2021-08-24 06:32:14 -0500

    Cambios en
    Variables de fila, columnas

--- Estego4bits.m
+++ Estego4bits.m
@@ -1,8 +1,9 @@
 %Fecha: 20/08/2021
+%Fecha Actualización:23/08/2021
 %Realiza estegonagráfia de imagen sobre imagen
-%LSB em 4 bits
-%Imagenes de mismo tamaño
-%MATLAB
+%LSB de 4 bits
+%Imagenes en gris de mismo tamaño
+%Código MATLAB
 
 %limpia pantalla;
 %Borra variables;
@@ -15,80 +16,68 @@ disp(mesaje01);
 ImgCover=imread('5.1.09.tiff');
 ImgSecrete=imread('5.1.12.tiff');
 
+[numf,numc]=size(ImgCover);
 
 VectCover=[];
-for i=1:256
-    for j=1:256
+for i=1:numf
+    for j=1:numc
         VectCover=[VectCover; ImgCover(i,j)];
     end
 end
 
 VectSecrete=[];
-for i=1:256
-    for j=1:256
+for i=1:size(ImgSecrete,1)
+    for j=1:size(ImgSecrete,1)
         VectSecrete=[VectSecrete; ImgSecrete(i,j)];
     end
 end
 
 %Enmascaramiento de los 4 bits má significativos de imagen cubierta
 VectCover2=[];
-for i=1:65536
+%240='1111 0000'
+for i=1:numf*numc
     VectCover2=[VectCover2; bitand(VectCover(i),240)];
 end
 
-%VectBinCover=dec2bin(VectCover);
-%VectBinSecrete=dec2bin(VectSecrete);
-%VectBinCover2=dec2bin(VectCover2);
 
 %Enmascaramiento de los 4 bits má significativos de imagen secreta
 VectSecrete2=[];
-for i=1:65536
+for i=1:numf*numc
     VectSecrete2=[VectSecrete2; bitand(VectSecrete(i),240)];
 end
 
 
-%VectBinSecrete2=dec2bin(VectSecrete2);
-
 %Corrimiento de los bits más significativos de la imagen secreta
-for i=1:65536
+for i=1:numf*numc
   VectSecrete3(i)=bitshift(VectSecrete2(i),-4);
 end
 
-%VectBinSecrete3=dec2bin(VectSecrete3);
-
 %Generación de la Estegoimagen
-for i=1:65536
+for i=1:numf*numc
     VectEstego(i)=bitor(VectCover2(i),VectSecrete3(i));
 end
 
 VectEstego=VectEstego';
-%VectBinEstego=dec2bin(VectEstego);
-
-%Formación de la matriz de imagen
 ImgEstego=[];
-for i=1:256
-    for j=1:256
-        ImgEstego(i,j)=VectEstego((i-1)*256+j);
+for i=1:numf
+    for j=1:size(ImgCover,2)
+        ImgEstego(i,j)=VectEstego((i-1)*numf+j);
     end
 end
 
 ImgEstego=uint8(ImgEstego);
 
 figure(1)
-title('Imagen cubierta')
 imshow(ImgCover);
+title('Imagen cubierta')
 
 figure(2)
-title('Imagen secreta')
 imshow(ImgSecrete);
-
 title('Imagen secreta')
+
 figure(3)
 imshow(ImgEstego);
-
-
-
-
+title('Imagen Estego')
 
 
 
